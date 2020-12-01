4b592e945234e6208e2e1ffcc1a3073992b0e4d9
Ruth Njeri <ruthnwaiganjo@gmail.com>
2020-12-01 09:49:34 +0300

    dp tabulation

--- Udemy/dynamic_programming/fibonacci.rb
+++ Udemy/dynamic_programming/fibonacci.rb
@@ -10,7 +10,7 @@ end
 # Dynamic solution
 # Need Overlapping Subproblem and Optimal Substructure
 # Remember and store answers to previous subproblems
-# Memoization: Store results of expensive function calls
+# Memoization(Top-Down): Store results of expensive function calls
 # and return the cached result when the same inputs occur again
 
 def fibonacci_memo(position, memo = [0, 1, 1])
@@ -22,4 +22,17 @@ def fibonacci_memo(position, memo = [0, 1, 1])
   result
 end
 
-# DP using Tabulation
+# DP using Tabulation(Bottom Up)
+# Better space complexity
+
+def fibonacci_table(position)
+  return 1 if position == 1 || position == 2
+
+  fib_table = [0, 1, 1]
+
+  (3..position).map do |index|
+    fib_table[index] = fib_table[index - 1] + fib_table[index - 2]
+  end
+
+  fib_table[position]
+end
