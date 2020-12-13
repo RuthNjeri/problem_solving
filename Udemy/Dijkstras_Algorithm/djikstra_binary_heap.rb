e1645a52ba1ea211d47a81bf2fc8429760806de7
Ruth Njeri <ruthnwaiganjo@gmail.com>
2020-12-13 21:17:18 +0300

    Djikstra upgraded

--- Udemy/Dijkstras_Algorithm/djikstra_binary_heap.rb
+++ Udemy/Dijkstras_Algorithm/djikstra_binary_heap.rb
@@ -1,26 +1,4 @@
 require 'pry'
-
-class PriorityQueue
-  attr_accessor :values
-
-  def initialize
-    @values = []
-  end
-
-  def enqueue(val, priority)
-    @values.push({val: val, priority: priority}) # [{ val: 'A', priority: 1}, { val: 'B', priority: 2}, { val: 'C', priority: 3}]
-    sort_values
-  end
-
-  def dequeue
-    @values.shift
-  end
-
-  def sort_values
-    @values.sort! {| a, b| a[:priority] <=> b[:priority]}
-  end
-end
-
 class WeightedGraph
   attr_accessor :adjacency_list
 
@@ -57,10 +35,9 @@ class WeightedGraph
       end
       previous[vertex] = nil
     end
-
     # As long as there is something to visit
     while nodes.values.length.positive?
-      smallest = nodes.dequeue[:val]
+      smallest = nodes.dequeue.value
       if smallest == finish
         while previous[smallest]
           path.push(smallest)
@@ -68,7 +45,6 @@ class WeightedGraph
         end
         break
       end
-
       if smallest || distances[smallest] != Infinity
         for neighbor in @adjacency_list[smallest]
           # calculate new distances to neighboring node
@@ -87,6 +63,82 @@ class WeightedGraph
 
     end
     p path.push(smallest).reverse
+  end
+end
+
+class PriorityQueue
+  attr_accessor :values
+  def initialize(values = [])
+    @values = values
+  end
+
+  def enqueue(value, priority) # Add node to values
+    node = Node.new(value, priority)
+    @values.push(node)
+    bubble_up
+  end
+
+  def bubble_up
+    index = @values.length - 1
+    parent_index = ( index - 1 ) / 2
+    parent_priority = @values[parent_index].priority
+    index_priority = @values[index].priority
+
+    while parent_priority > index_priority && !index.zero?
+      @values[parent_index], @values[index] = @values[index], @values[parent_index]
+
+      index = parent_index
+      parent_index = (index - 1) / 2
+      parent_priority = @values[parent_index].priority
+      index_priority = @values[index].priority
+    end
+    @values
+  end
+
+  def dequeue  # Remove node with highest priority
+    highest_priority = @values.first
+    lowest_priority = @values.pop
+    length = @values.length
+    if length > 0
+      @values[0] = lowest_priority
+      sink_down
+    end
+    highest_priority
+  end
+
+  def sink_down
+    parent_idx = 0
+    length = @values.length
+    parent = @values[parent_idx]
+
+    loop do
+      left_child_idx = 2 * parent_idx + 1
+      right_child_idx = 2 * parent_idx + 2
+      swap = false
+      if left_child_idx < length
+        swap = left_child_idx if @values[left_child_idx].priority > parent.priority
+      end
+      if right_child_idx < length
+        right_priority = @values[right_child_idx].priority
+        if swap == false && right_priority < parent.priority || swap != false && right_priority > @values[left_child_idx].priority
+          swap = right_child_idx
+        end
+      end
+      break unless swap
+
+      @values[parent_idx] = @values[swap]
+      @values[swap] = parent
+      parent_idx = swap
+    end
+
+  end
+end
+
+class Node
+  attr_reader :value, :priority
+  def initialize(value, priority)
+      @value = value
+      @priority = priority
   end
 end
 
@@ -110,4 +162,4 @@ graph.add_edge("D", "E", 3)
 graph.add_edge("D", "F", 1)
 graph.add_edge("E", "F", 1)
 
-graph.djikstra("A", "D")
+graph.djikstra("A", "E")
