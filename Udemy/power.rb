902d8ced06009ad0addd89ac70862539ae8add58
Ruth Njeri <ruthnwaiganjo@gmail.com>
2020-12-28 11:09:59 +0300

    subsequent characters

--- Udemy/power.rb
+++ Udemy/power.rb
@@ -1,5 +1,4 @@
 def power(base, exponent)
     return 1 if exponent == 0
-  
     base * power(base, exponent - 1)
 end
--- Udemy/problem_sets/frequency_counter_multiple_pointer/is_subsequence.rb
+++ Udemy/problem_sets/frequency_counter_multiple_pointer/is_subsequence.rb
@@ -1,3 +1,32 @@
+# Takes in two strings,
+# Checks whether the characters in the first string
+# Form a subsequence of characters in the second string
 
-def is_subsequence()
+require 'pry'
+def is_subsequence(string_1, string_2)
+
+  pointer_1 = 0
+  pointer_2 = 0
+
+  # Validation
+
+  return false if string_2.length < string_1.length
+  return false if string_1.length.zero? || string_2.length.zero?
+  return false if string_1.class != String || string_2.class != String
+
+  while pointer_1 < string_1.length && pointer_2 < string_2.length
+    if string_1[pointer_1] == string_2[pointer_2]
+      pointer_1 += 1
+    end
+    pointer_2 += 1
+
+    return true if pointer_1 == string_1.length
+  end
+
+  false
 end
+
+p is_subsequence("hello", "hello world")
+p is_subsequence("sing", "sting")
+p is_subsequence("abc", "abracadabra")
+p is_subsequence("abc", "acb")
