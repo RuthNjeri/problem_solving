a0e70146d251e569fcd45c93ca5432d3095c79ce
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-01-18 11:44:52 +0300

    fib solutions

--- AlgE/E/nth_fibonacci.rb
+++ AlgE/E/nth_fibonacci.rb
@@ -1,22 +1,31 @@
-# Recursive
+require 'pry'
 
+# Recursive
+# O(2^n)T
+# O(n)S
 # def nth_fib(n)
 #   return 0 if n == 1
-#   return 1 if n == 2 || n == 3
+#   return 1 if n == 2
 
 #   nth_fib(n-1) + nth_fib(n-2)
+# end
+
+# O(n)ST
+# def nth_fib(n, memoize = {1 => 0, 2 => 1})
+#   return memoize[n] if memoize[n]
+
+#   memoize[n] = nth_fib(n - 1, memoize) + nth_fib(n - 2 , memoize)
 # end
 
 
 
 # Iterative solution
-
+# O(n)T, O(n) S
 def nth_fib(n)
   prev_two = [0, 1]
 
   return prev_two[0] if n == 1
   return prev_two[1] if n == 2
-
   n -= 2
 
   while !n.zero?
@@ -27,6 +36,25 @@ def nth_fib(n)
   end
   prev_two.pop
 end
+
+# Iterative solution
+
+# def nth_fib(n)
+#   prev_two = [0, 1]
+
+#   return prev_two[0] if n == 1
+#   return prev_two[1] if n == 2
+
+#   counter = 3
+
+#   while counter <= n
+#     current_sum = prev_two[0] + prev_two[1]
+#     prev_two[0] = prev_two[1]
+#     prev_two[1] = current_sum
+#     counter += 1
+#   end
+#   prev_two.pop
+# end
 
 p nth_fib(2) # 1
 p nth_fib(1) # 0
